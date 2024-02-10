937a1c46fec86eee4547156b3a6fd448d83a345f
Chris A <ceaphx19401@yahoo.com>
2024-02-09 23:59:23 -0700

    initial lazyvim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+    -- add nordic
+    { "AlexvZyl/nordic.nvim" },
+
+    -- Configure LazyVim to load nordic
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "nordic",
+        },
+    },
+}
